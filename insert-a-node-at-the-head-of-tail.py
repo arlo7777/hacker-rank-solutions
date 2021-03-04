ebf02cf180af810ad2703de479fd30fe0650c969
Arlo Parker <50846350+arlo7777@users.noreply.github.com>
2021-03-04 13:35:37 -0500

    Create insert-a-node-at-the-head-of-tail.py

---
+++ insert-a-node-at-the-head-of-tail.py
@@ -0,0 +1,61 @@
+# Practice / Data Structures / Linked Lists / Insert a node at the head of a linked list
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtHead function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtHead(head, data):
+    node = SinglyLinkedListNode(data) #create a new node
+    
+    if head != None: #this is for the second element forward
+        node.next = head #insert a node before the existing node
+    
+    return node #this is the case when it returns the first node in the list and then                     the following nodes after 
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist_head = insertNodeAtHead(llist.head, llist_item)
+        llist.head = llist_head
+    
+    print_singly_linked_list(llist.head, '\n', fptr)
+    fptr.write('\n')
+    
+    fptr.close()
