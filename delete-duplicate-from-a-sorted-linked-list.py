4bc05041f828bec82071affda6cecfd143f94e54
Arlo Parker <50846350+arlo7777@users.noreply.github.com>
2021-03-07 10:50:51 -0500

    Create delete-duplicate-from-a-sorted-linked-list.py

---
+++ delete-duplicate-from-a-sorted-linked-list.py
@@ -0,0 +1,79 @@
+# Practice / Data Structures / Linked Lists / Delete duplicate-value nodes from a sorted linked list
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the removeDuplicates function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def removeDuplicates(head):
+    temp = head
+    while temp.next!=None:
+        if temp.data==temp.next.data: #check for the condition 
+            temp.next = temp.next.next #delete the next node
+        else:
+            temp=temp.next #if two nodes are not the same move to the next node
+    return head 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        llist1 = removeDuplicates(llist.head)
+
+        print_singly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
